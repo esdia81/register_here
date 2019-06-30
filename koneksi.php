ebdb2840a5f14f53737e89e45aeb921a1ac10c80
esdia81 <50251588+esdia81@users.noreply.github.com>
2019-07-01 02:29:08 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,6 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","registrasi2");
+//$koneksi = mysqli_connect("localhost","root","","registrasi2");
+$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {}, NULL, NULL); mysqli_real_connect($con, "esdiamysql.mysql.database.azure.com", "esdia@esdiamysql", {"sandibaro3.mysql"}, {"registrasi2"}, 3306);
  
 // Check connection
 if (mysqli_connect_errno()){
